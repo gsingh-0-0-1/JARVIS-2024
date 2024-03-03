7e2775964b52e236df559a0fe55dad898af30322
gsingh-0-0-1 <gurmehar@gmail.com>
2024-03-03 15:02:34 -0500

    Revert "minimap v1 lmcc"
    This reverts commit d223248523b647f2ccd91b14a12448fddd42b8d0.

--- src/lmcc/lmcc.js
+++ src/lmcc/lmcc.js
@@ -312,7 +312,7 @@ function generateBreadcrumbs() {
                 ws.send(JSON.stringify(breadcrumb));
             })
             .catch(error => console.error('Error generating breadcrumb:', error));
-    }, 1000); // 10 seconds interval
+    }, 10000); // 10 seconds interval
 }
 
 
