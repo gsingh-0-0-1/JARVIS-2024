bdd234f1c7a2ce5e9523c88b7c33cb211975c94d
gsingh-0-0-1 <gurmehar@gmail.com>
2024-05-14 20:58:23 -0700

    fix comm handlers

--- src/lmcc/public/js/right.js
+++ src/lmcc/public/js/right.js
@@ -1,3 +1,38 @@
+function defineWebSocketHandlers() {
+    ws.onmessage = async function (event, isBinary) {
+        var data = await event.data.text();
+        var message = JSON.parse(data);
+        var message_type = message["type"];
+        console.log('Received ' + message_type + ' from ' + message["sender"]);
+
+        if (message_type == "GEOPIN") {
+            //alert("we got a geopin");
+            // console.log(message["content"]);
+            addGeoPin(message["content"]);
+        } else if (message_type == "BREADCRUMBS1") {
+            // Display the list of breadcrumbs
+            // breadcrumbList.innerHTML = '';
+            // console.log(message.content)
+            // message.content.forEach(breadcrumb => {
+            addBreadCrumb1(message.content)
+            /*
+            var li = document.createElement('li');
+            var coords = message.content.coords;
+            var desc = message.content.desc;
+            li.textContent = `${desc}: (${coords.x.toFixed(2)}, ${coords.y.toFixed(2)})`;
+            breadcrumbList.appendChild(li);
+            */
+            // });
+        }  else if (message_type == "BREADCRUMBS2") {
+            addBreadCrumb2(message.content)
+
+        }
+    };
+}
+
+defineWebSocketHandlers();
+
+
 var LOCAL_DATA = {}
 LOCAL_DATA["GEOPINS"] = [];
 LOCAL_DATA["BREADCRUMBS1"] = [];
