cd2b127d63dfab0d00a7f91966610b5d4f9d3214
Benj011 <bemini@purdue.edu>
2024-05-20 22:51:40 -0500

    extended bio panel

---
+++ src/hmd/JARVIS/Assets/ExtendBioPanel.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtendBioPanel : MonoBehaviour
+{
+    public GameObject extension; 
+    // Start is called before the first frame update
+    public void OnExtendClick()
+    {
+
+        if (extension.ActiveSelf == true)
+        {
+            extension.SetActive(false);
+        }
+        else
+        {
+            extension.SetActive(true);
+        }
+
+
+    }
+}
